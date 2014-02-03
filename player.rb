8bc86dc65b944dc030d3141a6a5b6fea14d8a1ad
KYN 3 <kyn3@KYNs-MacBook-Air-3.local>
2014-02-03 10:35:50 -0500

    634 score with few lines of code

---
+++ player.rb
@@ -0,0 +1,31 @@
+
+class Player
+  attr_accessor :warrior
+  def play_turn(warrior)
+    self.warrior = warrior
+    actions?
+  end
+ def actions?
+    if warrior.look[1].enemy? && warrior.look[2].stairs?
+      warrior.shoot!
+    elsif warrior.health == 20 && warrior.look[0].empty? && warrior.look[1].empty? && warrior.look[2].enemy? && warrior.look(:backward)[1].wall? && warrior.look(:backward)[0].empty?
+      warrior.walk!
+ 		elsif warrior.feel.enemy?
+ 			warrior.attack!
+ 		elsif warrior.feel.captive?
+    	warrior.rescue!
+    elsif warrior.look[1].captive?
+    	warrior.walk!
+    elsif warrior.look[2].enemy?
+			warrior.shoot!
+    elsif warrior.feel(:backward).empty? && warrior.look[1].enemy? && warrior.look[2].empty? && warrior.look(:backward)[1].empty? == false
+      warrior.walk!(:backward)
+    elsif warrior.feel(:backward).captive?
+    	warrior.rescue!(:backward)
+    elsif warrior.feel.wall?
+      warrior.pivot!
+    else
+      warrior.walk!
+    end
+  end 
+end
